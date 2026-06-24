7d32406627d13dd83d798133e2b391e554646d07
Chris Sousa <x24sousa@gmail.com>
2026-06-24 10:45:02 -0600

    cleaned up some route53 attribute references

--- route53.tf
+++ route53.tf
@@ -17,22 +17,22 @@ resource "aws_route53_zone" "route53_x24sousa" {
 resource "aws_route53_record" "AAAA" {
   name    = var.domain_x24
   type    = "AAAA"
-  zone_id = "Z05720531MSHK0AAJ0Q96"
+  zone_id = aws_route53_zone.route53_x24sousa.zone_id
   alias {
     evaluate_target_health = false
-    name                   = "d24gghrejfkfcv.cloudfront.net"
-    zone_id                = "Z2FDTNDATAQYW2"
+    name                   = aws_cloudfront_distribution.cloudfront_distro.domain_name
+    zone_id                = aws_cloudfront_distribution.cloudfront_distro.hosted_zone_id
   }
 }
 
 resource "aws_route53_record" "A" {
   name    = var.domain_x24
   type    = "A"
-  zone_id = "Z05720531MSHK0AAJ0Q96"
+  zone_id = aws_route53_zone.route53_x24sousa.zone_id
   alias {
     evaluate_target_health = false
-    name                   = "d24gghrejfkfcv.cloudfront.net"
-    zone_id                = "Z2FDTNDATAQYW2"
+    name                   = aws_cloudfront_distribution.cloudfront_distro.domain_name
+    zone_id                = aws_cloudfront_distribution.cloudfront_distro.hosted_zone_id
   }
 }
 
@@ -40,22 +40,22 @@ resource "aws_route53_record" "A_www" {
   #multivalue_answer_routing_policy = false
   name    = "www.${var.domain_x24}"
   type    = "A"
-  zone_id = "Z05720531MSHK0AAJ0Q96"
+  zone_id = aws_route53_zone.route53_x24sousa.zone_id
   alias {
     evaluate_target_health = false
-    name                   = "d24gghrejfkfcv.cloudfront.net"
-    zone_id                = "Z2FDTNDATAQYW2"
+    name                   = aws_cloudfront_distribution.cloudfront_distro.domain_name
+    zone_id                = aws_cloudfront_distribution.cloudfront_distro.hosted_zone_id
   }
 }
 
 resource "aws_route53_record" "AAAA_www" {
   name    = "www.${var.domain_x24}"
   type    = "AAAA"
-  zone_id = "Z05720531MSHK0AAJ0Q96"
+  zone_id = aws_route53_zone.route53_x24sousa.zone_id
   alias {
     evaluate_target_health = false
-    name                   = "d24gghrejfkfcv.cloudfront.net"
-    zone_id                = "Z2FDTNDATAQYW2"
+    name                   = aws_cloudfront_distribution.cloudfront_distro.domain_name
+    zone_id                = aws_cloudfront_distribution.cloudfront_distro.hosted_zone_id
   }
 }
 
@@ -71,7 +71,7 @@ resource "aws_route53_record" "SOA" {
   records = ["ns-411.awsdns-51.com. awsdns-hostmaster.amazon.com. 1 7200 900 1209600 86400"]
   ttl     = 900
   type    = "SOA"
-  zone_id = "Z05720531MSHK0AAJ0Q96"
+  zone_id = aws_route53_zone.route53_x24sousa.zone_id
 }
 
 ###### NO NEED TO TOUCH THESE RECORDS ######
@@ -80,7 +80,7 @@ resource "aws_route53_record" "NS" {
   records = ["ns-1286.awsdns-32.org.", "ns-1947.awsdns-51.co.uk.", "ns-411.awsdns-51.com.", "ns-803.awsdns-36.net."]
   ttl     = 172800
   type    = "NS"
-  zone_id = "Z05720531MSHK0AAJ0Q96"
+  zone_id = aws_route53_zone.route53_x24sousa.zone_id
 }
 
 
@@ -90,7 +90,7 @@ resource "aws_route53_record" "CNAME" {
   records = ["_ac8ed1ce82a7633834ab88fb2279a04b.jkddzztszm.acm-validations.aws."]
   ttl     = 300
   type    = "CNAME"
-  zone_id = "Z05720531MSHK0AAJ0Q96"
+  zone_id = aws_route53_zone.route53_x24sousa.zone_id
 }
 
 ###### NO NEED TO TOUCH THESE RECORDS ######
@@ -99,5 +99,5 @@ resource "aws_route53_record" "CNAME_www" {
   records = ["_ca733e76a34149128275a6ad6f9aa8fe.jkddzztszm.acm-validations.aws."]
   ttl     = 300
   type    = "CNAME"
-  zone_id = "Z05720531MSHK0AAJ0Q96"
+  zone_id = aws_route53_zone.route53_x24sousa.zone_id
 }
